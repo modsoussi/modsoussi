98f31ba3bd7a21ca2b52a04a5a5fe2236b0ff7ec
modsoussi <mohameddsoussi@gmail.com>
2023-02-22 21:43:58 -0500

    wip

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,11 @@
 const Index = () => (
-  <div>Ad Astra 🚀</div>
+  <div className="h-full w-full bg-gray-100 flex items-center justify-center">
+    <div className="shadow rounded-lg p-4 bg-white">
+      <div>
+        <h1 className="text-lg font-semibold">Ad Astra 🚀</h1>
+      </div>
+    </div>
+  </div>
 )
 
 export default Index;
