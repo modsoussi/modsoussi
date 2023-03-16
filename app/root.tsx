e5d71596c42a4e2b8419afbc6955e02e25bd14df
modsoussi <mohameddsoussi@gmail.com>
2023-03-15 20:54:31 -0400

    ignore cache

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ import fontStylesheetUrl from "./styles/font.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "modsoussi;",
   viewport: "width=device-width,initial-scale=1",
 });
 
